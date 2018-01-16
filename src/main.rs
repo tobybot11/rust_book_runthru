85a01bb70d04f7c238c5129ea76d69fc6fc0340f
Tobias Ford <toby.ford@pobox.com>
2018-01-16 13:16:35 -0500

    up to the 12.10 listing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });;
 
     println!("Searching for {}", config.query);
     println!("In File {}", config.filename);
@@ -24,11 +28,16 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    println!("Args {:?}", args);
+impl Config{
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        println!("Args {:?}", args);
 
-    let query = args[1].clone();
-    let filename = args[2].clone();
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config { query, filename }
+        Ok(Config { query, filename })
+    }
 }
