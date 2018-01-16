d1cbd2a46975a09fad5cd42d8ca4588aad565e8c
Tobias Ford <toby.ford@pobox.com>
2018-01-16 12:59:48 -0500

    got thru listing 12-6 section

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In File {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In File {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
@@ -19,11 +19,16 @@ fn main() {
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
     println!("Args {:?}", args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    let query = args[1].clone();
+    let filename = args[2].clone();
 
-    (query, filename)
+    Config { query, filename }
 }
