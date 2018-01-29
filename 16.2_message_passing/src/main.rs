ccdeaaac936237f7063eca3c3983426659485c3e
Tobias Ford <toby.ford@pobox.com>
2018-01-29 13:04:42 -0500

    added some clarity it what gets returned

--- 16.2_message_passing/src/main.rs
+++ 16.2_message_passing/src/main.rs
@@ -7,12 +7,27 @@ fn main() {
 
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = mpsc::Sender::clone(&tx);
     thread::spawn(move || {
         let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
+            String::from("thread1 -> hi"),
+            String::from("thread1 -> from"),
+            String::from("thread1 -> the"),
+            String::from("thread! -> thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("t2 -> more"),
+            String::from("t2 -> messages"),
+            String::from("t2 -> for"),
+            String::from("t2 -> you"),
         ];
 
         for val in vals {
@@ -23,5 +38,5 @@ fn main() {
 
     for received in rx {
         println!("Got: {}", received);
-    } 
+    }
 }
