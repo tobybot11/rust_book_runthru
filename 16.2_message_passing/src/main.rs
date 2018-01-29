6e8adb3f40fbc2b87561ca8171a0861b95a1f835
Tobias Ford <toby.ford@pobox.com>
2018-01-29 12:34:09 -0500

    inital code from 16-7

--- 16.2_message_passing/src/main.rs
+++ 16.2_message_passing/src/main.rs
@@ -1,3 +1,13 @@
+use std::thread;
+use std::sync::mpsc;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! messages will be passed");
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
 }
