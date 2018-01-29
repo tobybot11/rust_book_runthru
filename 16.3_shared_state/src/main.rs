c2b3a47b48df438b1c557df0f22b37fdf67b1d60
Tobias Ford <toby.ford@pobox.com>
2018-01-29 13:09:05 -0500

    initial commit for 16-12 listing

--- 16.3_shared_state/src/main.rs
+++ 16.3_shared_state/src/main.rs
@@ -1,3 +1,15 @@
+use std::sync::Mutex;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! we are going to share state!");
+
+    let m = Mutex::new(5);
+    println!("m = {:?}", m);
+    
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
 }
