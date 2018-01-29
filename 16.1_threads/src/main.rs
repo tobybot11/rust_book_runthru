02cd426a6a949c74ba111f75c083cb0c0ba15edd
Tobias Ford <toby.ford@pobox.com>
2018-01-29 12:17:13 -0500

    up to listing 16-5

--- 16.1_threads/src/main.rs
+++ 16.1_threads/src/main.rs
@@ -14,4 +14,14 @@ fn main() {
     }
 
     // handle.join();
+
+    let v = vec![1,2,3];
+
+    let handle = thread::spawn(move || {
+       println!("Here's a vector: {:?}", v) ;
+    });
+
+    // drop(v);
+
+    handle.join();
 }
